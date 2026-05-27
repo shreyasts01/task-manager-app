481120d577a3772fda2fece54d9d0caf7f059a81
SHREYAS T S <tsshreyas4@gmail.com>
2026-05-28 02:06:36 +0530

    Change API base URL to production endpoint

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://task-manager-api-ulk4.onrender.com/api",
 });
 
 export default API;
