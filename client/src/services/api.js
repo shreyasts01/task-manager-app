02a6b2726c9a9a50fdd871b51529b4d54c1d9d20
SHREYAS T S <tsshreyas4@gmail.com>
2026-05-28 02:17:30 +0530

    Implement request interceptor for API calls
    Added request interceptor to include authorization token.

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,18 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://task-manager-api-ulk4.onrender.com/api",
+  baseURL:
+    "https://task-manager-api-ulk4.onrender.com/api",
+});
+
+API.interceptors.request.use((req) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    req.headers.authorization = token;
+  }
+
+  return req;
 });
 
 export default API;
